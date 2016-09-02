8c26f71065c823847c0c34b6229d71697aeafd49
Mike Winkelmann <mikedotwinkelmann@gmail.com>
2016-09-02 21:01:50 +0200

    * removed  unused script

--- cd/release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    git checkout master
-    mvn release:prepare -B --settings cd/mvnsettings.xml
-    mvn release:perform --settings cd/mvnsettings.xml
-fi
